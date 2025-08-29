4c2af8f6a2c50140c96d3d217221659619a15e4d
June823 <idahjunecherotich@gmail.com>
2025-08-29 16:43:48 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,10 @@
+-- =====================================================
+-- Assignment: Introduction to Databases
+-- Student Name: [June Oyugi]
+-- =====================================================
+
+-- Question 1: Write an SQL query to create a new database called salesDB
+CREATE DATABASE salesDB;
+
+-- Question 2: Write an SQL query to drop (delete) the database called demo
+DROP DATABASE demo;
